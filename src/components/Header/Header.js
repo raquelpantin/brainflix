86d4bb6ba73929f81c2bf222dfb3fa2d61bd4b2d
Raquel Pantin <raquel.pantin@gmail.com>
2022-02-23 13:52:54 -0500

    added search bar and search bar icon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,13 @@
 import "./Header.scss";
 import logo from "../../assets/images/logo/BrainFlix-logo.svg";
+import searchIcon from "../../assets/images/icons/search.svg";
 
 const Header = (props) => {
   return (
     <header className="header">
       <img className="header__logo" src={logo} alt="Brainflix Logo"></img>
+      <input className="header__searchbar" placeholder="Search"></input>
+      <img className="header__search-icon" src={searchIcon}></img>
     </header>
   );
 };
