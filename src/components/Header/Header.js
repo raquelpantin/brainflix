d5ea3f430a85003b037087ea04a18b210afdd3b4
Raquel Pantin <raquel.pantin@gmail.com>
2022-02-23 14:21:36 -0500

    added upload button with icon

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,22 @@
 import "./Header.scss";
 import logo from "../../assets/images/logo/BrainFlix-logo.svg";
 import searchIcon from "../../assets/images/icons/search.svg";
+import avatar from "../../assets/images/Mohan-muruge.jpg";
+import upload from "../../assets/images/icons/upload.svg";
 
 const Header = (props) => {
   return (
     <header className="header">
       <img className="header__logo" src={logo} alt="Brainflix Logo"></img>
-      <input className="header__searchbar" placeholder="Search"></input>
-      <img className="header__search-icon" src={searchIcon}></img>
+      <div className="header__search-container">
+        <input className="header__searchbar" placeholder="Search" />
+        <img className="header__search-icon" src={searchIcon}></img>
+      </div>
+      <img className="header__avatar" src={avatar}></img>
+      <div className="header__upload-container">
+        <button className="header__button">UPLOAD</button>
+        <img className="header__upload-icon" src={upload}></img>
+      </div>
     </header>
   );
 };
