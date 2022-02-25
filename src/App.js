907ad9c90e8f55071180526dc1fe16f67c31c094
Raquel Pantin <raquel.pantin@gmail.com>
2022-02-25 00:05:02 -0500

    imported necessary data to js file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header/Header.js";
 import VideoPlayer from "./components/VideoPlayer/VideoPlayer.js";
 import CommentForm from "./components/CommentForm/CommentForm.js";
+import CommentList from "./components/CommentList/CommentList.js";
 import "./App.css";
 import React from "react";
 
@@ -11,6 +12,7 @@ class App extends React.Component {
         <Header />
         <VideoPlayer />
         <CommentForm />
+        <CommentList />
       </>
     );
   }
--- src/components/CommentList/CommentList.js
+++ src/components/CommentList/CommentList.js
@@ -1,2 +1,30 @@
+import { Component } from "react";
+import Comments from "../../data/video-details.json";
 import "./CommentList.scss";
 import "../../App.scss";
+
+class CommentList extends Component {
+  state = { comments: Comments };
+  // JS code here
+
+  render() {
+    return (
+      <>
+        {this.state.comments.map((comment) => (
+          <article>
+            <img></img>
+            <div>
+              <ul>
+                <li></li>
+                <li></li>
+              </ul>
+              <p>{comment.title}</p>
+            </div>
+          </article>
+        ))}
+      </>
+    );
+  }
+}
+
+export default CommentList;
