845aaba96345506d88c56cbaa3914fa95aefbca8
Raquel Pantin <raquel.pantin@gmail.com>
2022-02-24 19:56:51 -0500

    updated responsiveness for tablet+ view

--- src/components/CommentForm/CommentForm.js
+++ src/components/CommentForm/CommentForm.js
@@ -8,14 +8,18 @@ const CommentForm = () => {
     <section className="commentBox">
       <img className="commentBox__avatar" src={avatar}></img>
       <form className="commentBox__form" method="get">
-        <label for="text">JOIN THE CONVERSATION</label>
-        <textarea
-          className="commentBox__text"
-          name="text"
-          placeholder="Add a new comment"
-        ></textarea>
-        <input className="commentBox__button" type="submit" value="COMMENT" />
-        <img className="commentBox__button--icon" src={commentIcon}></img>
+        <div className="commentBox__text-container">
+          <label for="text">JOIN THE CONVERSATION</label>
+          <textarea
+            className="commentBox__text"
+            name="text"
+            placeholder="Add a new comment"
+          ></textarea>
+        </div>
+        <div className="commentBox__button-container">
+          <input className="commentBox__button" type="submit" value="COMMENT" />
+          <img className="commentBox__button--icon" src={commentIcon}></img>
+        </div>
       </form>
     </section>
   );
