fb0ce299f3b90f519aa544404bb2df9660a49d7d
Raquel Pantin <raquel.pantin@gmail.com>
2022-02-26 12:22:16 -0500

    added video info section

---
+++ src/components/VideoInfo/VideoInfo.js
@@ -0,0 +1,27 @@
+import "./VideoInfo.scss";
+import viewsIcon from "../../assets/images/icons/views.svg";
+import likesIcon from "../../assets/images/icons/likes.svg";
+
+export default function VideoInfo({ currentVideo }) {
+  return (
+    <section>
+      <h1>{currentVideo.title}</h1>
+      <div>
+        <h2>{currentVideo.channel}</h2>
+        <p>{currentVideo.timestamp}</p>
+      </div>
+      <div>
+        <div>
+          <img src={viewsIcon}></img>
+          <p>{currentVideo.views}</p>
+        </div>
+        <div>
+          <img src={likesIcon}></img>
+          <p>{currentVideo.likes}</p>
+        </div>
+      </div>
+      <p>{currentVideo.description}</p>
+      <p>{currentVideo.comments.length} Comments</p>
+    </section>
+  );
+}
