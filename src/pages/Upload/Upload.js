1d482f48de9b5bf3d72e7e4800091bbe7e63e877
Raquel Pantin <raquel.pantin@gmail.com>
2022-03-05 18:46:53 -0500

    added notification system after upload submission

--- src/pages/Upload/Upload.js
+++ src/pages/Upload/Upload.js
@@ -1,57 +1,118 @@
 import "./Upload.scss";
 import thumbnail from "../../assets/images/Upload-video-preview.jpg";
 import publishIcon from "../../assets/images/icons/publish.svg";
+import React from "react";
 
-export default function Upload() {
-  return (
-    <section className="upload">
-      <h1 className="upload__title">Upload Video</h1>
-      <form className="upload__form">
-        <div className="upload__container">
-          <div className="upload__thumbnail">
-            <label className="upload__label-thumbnail">VIDEO THUMBNAIL</label>
-            <img
-              className="upload__thumbnail-image"
-              src={thumbnail}
-              alt="thumbnail"
-            />
-          </div>
-          <div className="upload__input">
-            <div className="upload__input-container">
-              <label className="upload__label" htmlFor="title">
-                TITLE YOUR VIDEO
-              </label>
-              <input
-                className="upload__input-title"
-                name="title"
-                placeholder="Add a title to your video"
-              ></input>
+export default class Upload extends React.Component {
+  state = {
+    title: "",
+    description: "",
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  isTitleValid = () => {
+    if (this.state.title.length < 1) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  isDescriptionValid = () => {
+    if (this.state.description.length < 1) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  isFormValid = () => {
+    if (!this.state.title || !this.state.description) {
+      return false;
+    }
+    if (!this.isTitleValid()) {
+      return false;
+    }
+    if (!this.isDescriptionValid()) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    if (this.isFormValid()) {
+      alert("Upload Successful! Redirecting back to the Home Page");
+      this.props.history.push("/");
+    } else {
+      alert("Upload must have a Title and Description");
+    }
+  };
+
+  render() {
+    return (
+      <section className="upload">
+        <h1 className="upload__title">Upload Video</h1>
+        <form onSubmit={this.handleSubmit} className="upload__form">
+          <div className="upload__container">
+            <div className="upload__thumbnail">
+              <label className="upload__label-thumbnail">VIDEO THUMBNAIL</label>
+              <img
+                className="upload__thumbnail-image"
+                src={thumbnail}
+                alt="thumbnail"
+              />
             </div>
-            <div className="upload__description-container">
-              <label className="upload__label" htmlFor="description">
-                ADD A VIDEO DESCRIPTION
-              </label>
-              <textarea
-                className="upload__input-description"
-                name="description"
-                placeholder="Add a description to your video"
-              ></textarea>
+            <div className="upload__input">
+              <div className="upload__input-container">
+                <label className="upload__label" htmlFor="title">
+                  TITLE YOUR VIDEO
+                </label>
+                <input
+                  className="upload__input-title"
+                  name="title"
+                  onChange={this.handleChange}
+                  value={this.state.title}
+                  placeholder="Add a title to your video"
+                ></input>
+              </div>
+              <div className="upload__description-container">
+                <label className="upload__label" htmlFor="description">
+                  ADD A VIDEO DESCRIPTION
+                </label>
+                <textarea
+                  className="upload__input-description"
+                  name="description"
+                  onChange={this.handleChange}
+                  value={this.state.description}
+                  placeholder="Add a description to your video"
+                ></textarea>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="upload__button-container">
-          <p className="upload__cancel-tablet">CANCEL</p>
-          <div className="upload__button-icon--position">
-            <button className="upload__button">PUBLISH</button>
-            <img
-              className="upload__button-icon"
-              src={publishIcon}
-              alt="publish icon"
-            />
+          <div className="upload__button-container">
+            <p className="upload__cancel-tablet">CANCEL</p>
+            <div className="upload__button-icon--position">
+              <button className="upload__button" type="submit">
+                PUBLISH
+              </button>
+              <img
+                className="upload__button-icon"
+                src={publishIcon}
+                alt="publish icon"
+              />
+            </div>
+            <p className="upload__cancel">CANCEL</p>
           </div>
-          <p className="upload__cancel">CANCEL</p>
-        </div>
-      </form>
-    </section>
-  );
+        </form>
+      </section>
+    );
+  }
 }
