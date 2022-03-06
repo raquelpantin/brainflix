d188c0fb1995480bbffa84469d03ae99a3c39b83
Raquel Pantin <raquel.pantin@gmail.com>
2022-03-06 13:38:13 -0500

    removed commented out code

--- src/App.js
+++ src/App.js
@@ -23,36 +23,3 @@ function App() {
 }
 
 export default App;
-
-// import { Component } from "react";
-// import "./App.scss";
-// import Main from "./components/Main/Main";
-// import videoData from "./data/video-details.json";
-
-// class App extends Component {
-//   state = {
-//     videoData: videoData,
-//     currentVideo: videoData[0],
-//   };
-
-//   nextVideo = (id) => {
-//     const nextVideoId = this.state.videoData.findIndex(
-//       (video) => id === video.id
-//     );
-//     this.setState({
-//       currentVideo: this.state.videoData[nextVideoId],
-//     });
-//   };
-
-//   render() {
-//     return (
-//       <Main
-//         videoData={this.state.videoData}
-//         currentVideo={this.state.currentVideo}
-//         nextVideo={this.nextVideo}
-//       />
-//     );
-//   }
-// }
-
-// export default App;
